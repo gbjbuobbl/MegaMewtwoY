4232fd1440a984a5e9013f13193e71b1fd95af73
gbjbuobbl <79749963+gbjbuobbl@users.noreply.github.com>
2021-07-09 14:57:33 +0530

    Add files via upload

---
+++ kwww.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDRP06ozxP6t8LGqvSryTP0JY0uO5iEdUY",
+    authDomain: "naruto-kun-dattebayo.firebaseapp.com",
+    databaseURL: "https://naruto-kun-dattebayo-default-rtdb.firebaseio.com",
+    projectId: "naruto-kun-dattebayo",
+    storageBucket: "naruto-kun-dattebayo.appspot.com",
+    messagingSenderId: "981095120524",
+    appId: "1:981095120524:web:b5bc512d3cc54b6a4f05a9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
